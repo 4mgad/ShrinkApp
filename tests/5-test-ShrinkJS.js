84dde4943d61ac159006c58e98afcf70a96e2cb0
4mgad <ay@amgad.us>
2013-11-25 13:34:11 -0800

    Unit Testing Framework - applying changes

--- tests/5-test-ShrinkJS.js
+++ tests/5-test-ShrinkJS.js
@@ -1,108 +1,100 @@
-var TestSuite = {
-  run: function(callback) {
-    console.log('Testing ShrinkJS.js');
+var TestSuite = require("./TestSuite.js");
 
-    var fs = require("extendfs");
-    var Config = require("../lib/Config.js");
-    var ShrinkJS = require("../lib/filters/ShrinkJS.js");
+var fs = require("extendfs");
+var Config = require("../lib/Config.js");
+var ShrinkJS = require("../lib/filters/ShrinkJS.js");
 
+var g = {};
+
+var shrinkJSTS = new TestSuite("ShrinkJS.js", [
+  //0
+  function(callback) {
     var appConf = new Config();
     appConf.config(function(err) {
       if (err) {
-        console.log(err);
+        callback(err);
       } else {
-        var buildDir = appConf.getBuildDir();
-        var shrinkJS = new ShrinkJS.getInstance(appConf);
-
+        var buildDir = g.buildDir = appConf.getBuildDir();
+        g.shrinkJS = new ShrinkJS.getInstance(appConf);
         fs.deleteDir(buildDir, function(err, dir) {
-          if (err) {
-            console.log(err);
-          }
           fs.copyDir(__dirname + '/app', buildDir, function(err, src, dest) {
             if (err) {
-              console.log(err);
+              callback(err);
             } else {
-
-
-
-              var testCase1 = function() {
-                console.log('Test Case #1');
-                var validJsTxt = fs.readFileSync(buildDir + '/js/test-case-5-1.js', 'utf8');
-                shrinkJS.applyFilter(buildDir + '/js/app.js', function(err, minJSFiles) {
-                  if (err) {
-                    console.trace();
-                    console.log(err);
-                  } else {
-                    var jsTxt = fs.readFileSync(minJSFiles[0], 'utf8');
-
-                    if (jsTxt === validJsTxt) {
-                      console.log('SUCCESS!');
-                    } else {
-                      console.log(minJSFiles[0]);
-                      console.log('FAILED');
-                    }
-                  }
-                });
-              }();
-
-              var testCase2 = function() {
-                console.log('Test Case #2');
-                var validJsTxt = fs.readFileSync(buildDir + '/js/test-case-5-2.js', 'utf8');
-                shrinkJS.applyFilter([
-                  buildDir + '/js/app.js',
-                  buildDir + '/js/controllers.js',
-                  buildDir + '/js/directives.js',
-                  buildDir + '/js/filters.js',
-                  buildDir + '/js/services.js'
-                ], function(err, minJSFiles) {
-                  if (err) {
-                    console.log(err);
-                  } else {
-                    var jsTxt = fs.readFileSync(minJSFiles[0], 'utf8');
-
-                    if (jsTxt === validJsTxt) {
-                      console.log('SUCCESS!');
-                    } else {
-                      console.log(minJSFiles[0]);
-                      console.log('FAILED');
-                    }
-                  }
-                });
-              }();
-
-
-              var testCase3 = function() {
-                console.log('Test Case #3');
-                shrinkJS.finalize(function(err, arr) {
-                  if (err) {
-                    console.log(err);
-                  } else {
-                    var noneExist = true;
-                    for (var i = 0; i < arr.length; i++) {
-                      var file = arr[i];
-                      if (fs.existsSync(file)) {
-                        noneExist = false;
-                      }
-                    }
-                    if (arr.length && noneExist) {
-                      console.log('SUCCESS!');
-                      callback();
-                    } else {
-                      console.log('FAILED');
-                    }
-                  }
-                });
-              }();
-
-
-
+              callback();
             }
           });
         });
-
       }
     });
+  },
+  //1
+  function(callback) {
+    var buildDir = g.buildDir;
+    var shrinkJS = g.shrinkJS;
+    var validJsTxt = fs.readFileSync(buildDir + '/js/test-case-5-1.js', 'utf8');
+    shrinkJS.applyFilter(buildDir + '/js/app.js', function(err, minJSFiles) {
+      if (err) {
+        callback(err);
+      } else {
+        var jsTxt = fs.readFileSync(minJSFiles[0], 'utf8');
 
+        if (jsTxt === validJsTxt) {
+          callback();
+        } else {
+          callback('FAILED');
+        }
+      }
+    });
+  },
+  //2
+  function(callback) {
+    var buildDir = g.buildDir;
+    var shrinkJS = g.shrinkJS;
+    var validJsTxt = fs.readFileSync(buildDir + '/js/test-case-5-2.js', 'utf8');
+    shrinkJS.applyFilter([
+      buildDir + '/js/app.js',
+      buildDir + '/js/controllers.js',
+      buildDir + '/js/directives.js',
+      buildDir + '/js/filters.js',
+      buildDir + '/js/services.js'
+    ], function(err, minJSFiles) {
+      if (err) {
+        callback(err);
+      } else {
+        var jsTxt = fs.readFileSync(minJSFiles[0], 'utf8');
+
+        if (jsTxt === validJsTxt) {
+          callback();
+        } else {
+          callback('FAILED');
+        }
+      }
+    });
+  },
+  //3
+  function(callback) {
+    var shrinkJS = g.shrinkJS;
+    shrinkJS.finalize(function(err, arr) {
+      if (err) {
+        callback(err);
+      } else {
+        var noneExist = true;
+        for (var i = 0; i < arr.length; i++) {
+          var file = arr[i];
+          if (fs.existsSync(file)) {
+            noneExist = false;
+          }
+        }
+        if (arr.length && noneExist) {
+          callback();
+        } else {
+          callback('FAILED');
+        }
+      }
+    });
   }
-};
-module["exports"] = TestSuite;
+
+]);
+
+module["exports"] = shrinkJSTS;
