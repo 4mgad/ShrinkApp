dfc86c5dc18332874fb86b8d22fc7698cfc9e103
4mgad <ay@amgad.us>
2013-11-25 12:41:46 -0800

    Unit Testing Framework - initial phase

--- tests/test-All.js
+++ tests/test-All.js
@@ -1,15 +1,31 @@
-require('./1-test-Config.js').run(function() {
-  require('./2-test-Utils.js').run(function() {
-    require('./3-test-CompileLESS.js').run(function() {
-      require('./4-test-ShrinkCSS.js').run(function() {
-        require('./5-test-ShrinkJS.js').run(function() {
-          require('./6-test-ParseHTML.js').run(function() {
-            require('./7-test-ShrinkApp.js').run(function() {
-              console.log("ALL TEST SUITES SUCCEEEDED!");
-            });
-          });
-        });
-      });
+var formatMsg = require('./TestUtils.js').formatMsg;
+
+var testArr = [
+//  './1-test-Analysis.js',
+//  './1-test-Config.js',
+//  './2-test-Utils.js',
+//  './3-test-CompileLESS.js',
+  './4-test-ShrinkCSS.js',
+//  './5-test-ShrinkJS.js',
+//  './6-test-ParseHTML.js',
+//  './7-test-ShrinkApp.js'
+];
+
+var idx = 0;
+var runTestSuite = function(idx) {
+  var testSuite = testArr[idx];
+  if (testSuite) {
+    require(testSuite).run(function() {
+      runTestSuite(++idx);
     });
-  });
-});
+  } else {
+    console.log(" ");
+    console.log(" ");
+    if (idx === testArr.length) {
+      console.log(formatMsg("ALL TEST SUITES:", 30) + "SUCCEEDED!!!");
+    } else {
+      console.log(formatMsg("SOME TEST SUITES:", 30) + "FAILED");
+    }
+  }
+};
+runTestSuite(idx);
